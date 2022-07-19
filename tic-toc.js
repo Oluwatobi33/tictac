111a1645b80f1482e0cbeca66ca76c50bf87184e
Oluwatobi22 <bakareoluwatobi22@gmail.com>
2022-07-19 12:28:23 +0100

    added continue and start button

--- tic-toc.js
+++ tic-toc.js
@@ -9,53 +9,87 @@ const pau =()=>{
 const re = ()=>{
   location.reload()
 }
+ const now = ()=>{
+    var Fr =   fp.value;
+     var Sr = sp.value;
+     if (fp.value == "" && sp.value == "") {
+        err1.innerText = "Enter the value of firstplayer";
+        err1.style.color = "red";
+        err2.innerText = "Enter the value of firstplayer";
+        err2.style.color = "red";
+     }else if(fp.value !== "" && sp.value !== ""){
+        ft.innerText = fp.value;
+        lp.innerText = sp.value;
+
+     }
+ }
 let initialValue = "x"
 const sa = (e) => {
-    if (initialValue == "x" && e.target.innerText == "") {
-        e.target.innerText = "x";
-        initialValue = "o";
-    } else if (initialValue == "o" && e.target.innerText == "") {
-        e.target.innerText = "o";
-        initialValue = "x";
+    if (fp.value != "" && sp.value != "") {
+        if (initialValue == "x" && e.target.innerText == "") {
+            e.target.innerText = "x";
+            initialValue = "o";
+        } else if (initialValue == "o" && e.target.innerText == "") {
+            e.target.innerText = "o";
+            initialValue = "x";
+        }
+        let one = document.getElementById("one1");
+        let two = document.getElementById("one2");
+        let three = document.getElementById("one3");
+        let four = document.getElementById("two1");
+        let five = document.getElementById("two2");
+        let six = document.getElementById("two3");
+        let seven = document.getElementById("three1");
+        let eight = document.getElementById("three2");
+        let nine = document.getElementById("three3");
+    
+        let a = one.innerHTML;
+        let b = two.innerHTML;
+        let c = three.innerHTML;
+        let d = four.innerHTML;
+        let d1 = five.innerHTML;
+        let f = six.innerHTML;
+        let g = seven.innerHTML;
+        let h = eight.innerHTML;
+        let i = nine.innerHTML;
+    
+        if ((a == "x" && b == "x" && c == "x")||((d == "x" && d1 == "x" && f == "x"))|| ((g== "x" && h== "x" && i== "x"))) {
+            initialValue = ""
+            alert(ft.innerText + "" + " win round")
+        }
+        else if ((a == "o" && b == "o"&& c == "o") ||((d == "o" && d1 == "o" && f == "o"))|| ((g == "o" && h == "o" && i== "o")))
+        {
+            alert(lp.innerText + "" + " win round") 
+            initialValue = ""
+        } 
+        else if((a == "x" && d1 == "x" && i== "x") || ((a == "x" && d== "x" && g== "x"))  || ((b == "x" && d1 == "x" && h == "x" )) || ((c == "x" && f == "x" && i== "x" )) ||((c == "x" && d1 == "x" && g == "x"))){
+            alert(ft.innerText + "" + " win round");
+            initialValue = ""
+        }
+        else if((a == "o" && d1 == "o" && i== "o") || ((a == "o" && d== "o" && g== "o")) || ((b == "o" && d1 == "o" && h == "o" )) || ((c == "o" && f == "o" && i== "o" )) ||((c == "o" && d1 == "o" && g == "o"))){
+            alert(lp.innerText + "" + "win round");
+            initialValue = ""
+        } 
+    
+        let take = document.getElementById("score").innerText = ft.innerText
+        console.log(take);
+    }else{
+        alert("Enter your name")
     }
-    let one = document.getElementById("one1");
-    let two = document.getElementById("one2");
-    let three = document.getElementById("one3");
-    let four = document.getElementById("two1");
-    let five = document.getElementById("two2");
-    let six = document.getElementById("two3");
-    let seven = document.getElementById("three1");
-    let eight = document.getElementById("three2");
-    let nine = document.getElementById("three3");
-
-    let a = one.innerHTML;
-    let b = two.innerHTML;
-    let c = three.innerHTML;
-    let d = four.innerHTML;
-    let d1 = five.innerHTML;
-    let f = six.innerHTML;
-    let g = seven.innerHTML;
-    let h = eight.innerHTML;
-    let i = nine.innerHTML;
-
-    if ((a == "x" && b == "x" && c == "x")||((d == "x" && d1 == "x" && f == "x"))|| ((g== "x" && h== "x" && i== "x"))) {
-        alert(ft.innerText + " win round")
-    }
-    else if ((a == "o" && b == "o"&& c == "o") ||((d == "o" && d1 == "o" && f == "o"))|| ((g == "o" && h == "o" && i== "o")))
-    {
-        alert(lp.innerText + " win round") 
-    } 
-    else if((a == "x" && d1 == "x" && i== "x") || ((a == "x" && d== "x" && g== "x"))  || ((b == "x" && d1 == "x" && h == "x" )) || ((c == "x" && f == "x" && i== "x" )) ||((c == "x" && d1 == "x" && g == "x"))){
-        alert(ft.innerText + " win round");
-    }
-    else if((a == "o" && d1 == "o" && i== "o") || ((a == "o" && d== "o" && g== "o")) || ((b == "o" && d1 == "o" && h == "o" )) || ((c == "o" && f == "o" && i== "o" )) ||((c == "o" && d1 == "o" && g == "o"))){
-        alert(lp.innerText + "win round");
-    } 
-
-    let take = document.getElementById("score").innerText = ft.innerText
-    console.log(take);
+    
 }
-   const now = ()=>{
-   var Fr = ft.innerText = fp.value;
-   var Sr = lp.innerText = sp.value;
- }
+//    const now = ()=>{
+//    
+//  }
+ const ce = ()=>{
+    initialValue = "x";
+    one.innerHTML = "";
+    two.innerHTML;
+    three.innerHTML;
+    four.innerHTML;
+    five.innerHTML;
+    six.innerHTML;
+    seven.innerHTML;
+    eight.innerHTML;
+    nine.innerHTML;
+}
